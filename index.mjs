75b0f2b5129b33ee4a143e0de814d60d5f2c6ca2
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-06-16 23:10:51 +0530

    first commit

---
+++ index.mjs
@@ -0,0 +1,7 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello world!'));
+console.log(chalk.green.italic('This is a green message!'));
+console.log(chalk.red.bold('This is a bold red message!'));
+console.log(chalk.yellow.underline('This is a underlined yellow message!'));
+console.log(chalk.magenta.inverse('This is a inverse magenta message!'));
