7187b42e4b1decb2a3b9549dcb8f06d032a3e193
Nitin-cyberpunk <122776086+Nitin-cyperpunk@users.noreply.github.com>
2025-06-23 23:47:00 +0530

    handler

---
+++ http/main.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+
+let requestCount = 0;
+
+function requestIncrease(req, res, next) {
+    requestCount = requestCount + 1;
+    console.log("total number of requests = " + requestCount);
+    req.requestCount = requestCount;
+    res.json({
+       msg: "Request received" 
+    })
+}
+
+function sumHandler(req, res) {
+    const a = parseInt(req.params.a);
+    const b = parseInt(req.params.b);
+    res.json({
+        answer: a + b
+    });
+}
+app.get("/sum", requestIncrease, sumHandler);
+
+    
+ 
+
+app.get("/multi/:a/:b", function(req, res)  {
+    requestIncrease(req, res);
+ const a = parseInt(req.params.a);
+ const b = parseInt(req.params.b);
+ res.json({
+    answer: a * b
+ })
+
+})
+app.listen(3000)
--- http/middle.js
+++ http/middle.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+
+function loggerMiddleware(req, res, next) {
+    console.log("method is " + req.method);
+    console.log("Host is " + req.hostname);
+    console.log("Route is " + req.url);
+    console.log(new Date());
+    next;
+}
+app.use(loggerMiddleware);
 app.get("/sum/:a/:b", function(req, res)  {
  const a = parseInt(req.params.a);
  const b = parseInt(req.params.b);
@@ -15,20 +24,4 @@ app.get("/multi/:a/:b", function(req, res)  {
  })
 
 })
-app.get("/sub/:a/:b", function(req, res)  {
- const a = parseInt(req.params.a);
- const b = parseInt(req.params.b);
- res.json({
-    answer: a - b
- })
-
-})
-app.get("/div/:a/:b", function(req, res)  {
- const a = parseInt(req.params.a);
- const b = parseInt(req.params.b);
- res.json({
-    answer: a / b
- })
-
-})
-app.listen(3000);
+app.listen(3000) 
