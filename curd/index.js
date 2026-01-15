bb288347dd5784690992c8176b9b4afe5fa2ed1a
NitinCyper-punk <singhnitin9975@gmail.com>
2026-01-16 00:38:37 +0530

    learning curd

--- curd/index.js
+++ curd/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
-const users = require("./MOCK_DATA.json");
+
+const fs = require("fs");
 const app = express()
 
 const PORT = 8000
+let users = JSON.parse(fs.readFileSync("./MOCK_DATA.json", "utf-8"));
+app.use(express.json());
 
-
-app.get("/api/users",(req, res) => {
+app.use(express.urlencoded({ extended: false }))
+app.get("/api/users", (req, res) => {
    return res.json(users)
 })
 
@@ -25,28 +28,28 @@ app.get('/users', (req, res) => {
 //    const user = users.find((user) => user.id === id)
 //    return res.json(user)
 // })
-   
+
 
 app.route('/api/users/:id')
    .get((req, res) => {
       const id = Number(req.params.id)
       const user = users.find((user) => user.id === id)
       return res.json(user)
-   }).patch((req, res) => {
-      const id = Number(req.params.id)
-      let user = users.find((user) => user.id === id)
-      user = { ...user, ...req.body }
-      return res.json(user)
-   }  ).delete((req, res) => {
-      const id = Number(req.params.id)
-      const filteredUsers = users.filter((user) => user.id !== id)
-      return res.json(filteredUsers)
+   // }).patch((req, res) => {
+   //    const id = Number(req.params.id)
+   //    let user = users.find((user) => user.id === id)
+   //    user = { ...user, ...req.body }
+   //    return res.json(user)
+   // }).delete((req, res) => {
+   //    const id = Number(req.params.id)
+   //    const filteredUsers = users.filter((user) => user.id !== id)
+   //    return res.json(filteredUsers)
    })
 
-app.post("/api/users",(req, res) => {  
-   const body = req.body
-   console.log("Body", body);
-   
-      return res.json({message: "User created successfully"})
-   }  )
-app.listen(PORT,() => console.log(`Server is running on port ${PORT}`))
+app.post("/api/users", (req, res) => { 
+   const body = req.body;
+    users.push({ ...body, id: users.length + 1 });
+     fs.writeFile("./MOCK_DATA.json", JSON.stringify(users), "utf-8", (err, data) => { 
+      return res.json({ message: "User created successfully" }) }) })
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
